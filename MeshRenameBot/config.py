046dd6194d06c9bca5eeea89d6c76e3c73a9727b
sridhar885 <91075267+sridhar885@users.noreply.github.com>
2021-09-28 20:07:34 +0530

    Update config.py

--- MeshRenameBot/config.py
+++ MeshRenameBot/config.py
@@ -5,19 +5,19 @@ try:
 except ImportError:
     class Config:
         DATABASE_URL = [str, ""]
-        API_HASH = [str, "abcdedf......"]
-        API_ID = [int, 1234567]
-        BOT_TOKEN = [str, "bot:token here"]
+        API_HASH = [str, "89ebdbc6e7bf9195bde696b0e9b3c387"]
+        API_ID = [int, 8941261]
+        BOT_TOKEN = [str, "1998570339:AAGnc97dhnar2w6tITYpLegXdku4qhoys38"]
         COMPLETED_STR = [str, "▰"]
         REMAINING_STR = [str, "▱"]
-        MAX_QUEUE_SIZE = [int, 5]
+        MAX_QUEUE_SIZE = [int, 50]
         SLEEP_SECS = [int, 10]
         IS_MONGO = [bool, False]
 
         # Access Restriction
         IS_PRIVATE = [bool, False]
-        AUTH_USERS = [list,[123456789]]
-        OWNER_ID = [int, 0]
+        AUTH_USERS = [list,[2025727672]]
+        OWNER_ID = [int, 2025727672]
 
         # Public username url or invite link of private chat
         FORCEJOIN = [str,""]
